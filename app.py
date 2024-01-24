5ff641ce4d09a37e98bc1b21c42e5517fb660a5b
uros <uros.skrt666@gmail.com>
2024-01-24 16:42:36 +0100

    inital links prepared with arrays instad of databases

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Changed this string :D'
+events = ["dragon"];
+
+characters = [];
+
+@app.route('/loadEvents')
+def sendEvents():
+    return events
+
+@app.route("/loadCharacters")
+def sendCharacters():
+    return characters
+
+@app.route("/addCharacter")
+def addCharacter(charName):
+    characters.append(charName);
+    return sendCharacters();
+
 
 if __name__ == "__main__":
     app.run()
