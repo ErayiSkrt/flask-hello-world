460a325d3ce9ac369da19ad0665dbf71fcfeb3ea
Uros <uros.skrt@almamater.si>
2024-01-31 20:55:38 +0100

    finished add and remove character

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ CORS(app)
 
 events = [{"eventName": "dragon"}]
 
-characters = [];
+characters = [{"charName": "testName"}];
 
 @app.route('/loadEvents')
 def sendEvents():
@@ -18,9 +18,21 @@ def sendCharacters():
 @app.route("/addCharacter/<charName>")
 def addCharacter(charName):
     for name in characters:
-        if name == charName:
-            return;
+        if name["charName"] == charName:
+            return {"0":"character already exists"};
+
     characters.append({"charName": charName});
+    return {"0":"character added", "1":"adding"}
+
+@app.route("/removeCharacter/<charName>")
+def removeCharacter(charName):
+    global characters;
+    updatedList = [d for d in characters if d.get("charName") != charName]
+    if len(updatedList) < len(characters):
+        characters = updatedList;
+        return {"0":"character removed", "1": "removed"};
+
+    return {"0":"character with name " + charName + " does not exist"};
 
 if __name__ == "__main__":
     app.run()
